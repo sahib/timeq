ff70db77a4f0aecc81c12159aa609542204359bd
Chris Pahl <sahib@online.de>
2023-10-29 18:16:03 +0100

    chores: add some test for the copy utility

--- bucket/util_test.go
+++ bucket/util_test.go
@@ -5,10 +5,11 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/sahib/timeq/item/testutils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMoveFile(t *testing.T) {
+func TestMoveFileIfRenamable(t *testing.T) {
 	t.Parallel()
 
 	dir, err := os.MkdirTemp("", "timeq-copyfile")
@@ -28,4 +29,90 @@ func TestMoveFile(t *testing.T) {
 
 	_, err = os.Stat(aPath)
 	require.True(t, os.IsNotExist(err))
+}
+
+func TestMoveIfNotRenamable(t *testing.T) {
+	t.Parallel()
+
+	testutils.WithTempMount(t, func(ext4Dir string) {
+		tmpDir, err := os.MkdirTemp("", "timeq-copyfile")
+		require.NoError(t, err)
+		defer os.RemoveAll(tmpDir)
+
+		aData := []byte("Hello")
+		bData := []byte("World")
+		aPath := filepath.Join(tmpDir, "a")
+		bPath := filepath.Join(tmpDir, "b")
+		require.NoError(t, os.WriteFile(aPath, aData, 0600))
+		require.NoError(t, os.WriteFile(bPath, bData, 0600))
+
+		dstDir := filepath.Join(ext4Dir, "dst")
+		require.NoError(t, moveFileOrDir(tmpDir, dstDir))
+
+		entries, err := os.ReadDir(dstDir)
+		require.NoError(t, err)
+		require.Len(t, entries, 2)
+
+		expAData, err := os.ReadFile(filepath.Join(dstDir, "a"))
+		require.NoError(t, err)
+
+		expBData, err := os.ReadFile(filepath.Join(dstDir, "b"))
+		require.NoError(t, err)
+
+		require.Equal(t, aData, expAData)
+		require.Equal(t, bData, expBData)
+
+		_, err = os.Stat(aPath)
+		require.True(t, os.IsNotExist(err))
+		_, err = os.Stat(bPath)
+		require.True(t, os.IsNotExist(err))
+	})
+}
+
+func TestMoveErrorIfRenamable(t *testing.T) {
+	t.Parallel()
+
+	dir, err := os.MkdirTemp("", "timeq-copyfile")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	expData := []byte("Hello")
+	aPath := filepath.Join(dir, "a")
+	require.NoError(t, os.WriteFile(aPath, expData, 0600))
+
+	badDir := filepath.Join(dir, "bad")
+	require.NoError(t, os.MkdirAll(badDir, 0400))
+	require.Error(t, moveFileOrDir(aPath, badDir))
+
+	// Nothing should have been deleted:
+	_, err = os.Stat(aPath)
+	require.NoError(t, err)
+}
+
+func TestMoveErrorIfNotRenamable(t *testing.T) {
+	t.Parallel()
+
+	testutils.WithTempMount(t, func(ext4Dir string) {
+		tmpDir, err := os.MkdirTemp("", "timeq-copyfile")
+		require.NoError(t, err)
+		defer os.RemoveAll(tmpDir)
+
+		aData := []byte("Hello")
+		bData := []byte("World")
+		aPath := filepath.Join(tmpDir, "a")
+		bPath := filepath.Join(tmpDir, "b")
+		require.NoError(t, os.WriteFile(aPath, aData, 0600))
+		require.NoError(t, os.WriteFile(bPath, bData, 0600))
+
+		// Create dst dir as only-readable, not even cd-able dir:
+		dstDir := filepath.Join(ext4Dir, "dst")
+		require.NoError(t, os.MkdirAll(dstDir, 0400))
+
+		require.Error(t, moveFileOrDir(tmpDir, dstDir))
+
+		_, err = os.Stat(aPath)
+		require.NoError(t, err)
+		_, err = os.Stat(bPath)
+		require.NoError(t, err)
+	})
 }
--- item/testutils/testutils.go
+++ item/testutils/testutils.go
@@ -2,8 +2,15 @@ package testutils
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"syscall"
+	"testing"
 
 	"github.com/sahib/timeq/item"
+	"github.com/stretchr/testify/require"
 )
 
 func ItemFromIndex(idx int) item.Item {
@@ -32,4 +39,43 @@ func GenItems(start, stop, step int) item.Items {
 	}
 
 	return its
+}
+
+// WithTempMount calls `fn` with the path to directory that contains an empty
+// ext4 filesystem that will be unmounted once the test finished.
+func WithTempMount(t *testing.T, fn func(mountDir string)) {
+	if runtime.GOOS != "linux" {
+		t.Skipf("this test uses ext4 and other linux specific tools")
+	}
+
+	if os.Geteuid() != 0 {
+		t.Skipf("this test needs to be run with root permissions to work")
+	}
+
+	dir, err := os.MkdirTemp("", "mount-test")
+	require.NoError(t, err)
+	defer os.RemoveAll(dir)
+
+	// Create a file big enough to hold a small filesystem:
+	loopPath := filepath.Join(dir, "loop")
+	fd, err := os.Create(loopPath)
+	require.NoError(t, err)
+	require.NoError(t, fd.Truncate(1*1024*1024))
+	require.NoError(t, fd.Close())
+
+	// Create a filesystem in the loop file:
+	ext4Out, err := exec.Command("mkfs.ext4", loopPath).Output()
+	require.NoError(t, err, string(ext4Out))
+
+	// Mount the ext4 fs to a newly created directory:
+	mountDir := filepath.Join(dir, "mount")
+	require.NoError(t, os.MkdirAll(mountDir, 0600))
+	mountOut, err := exec.Command("mount", loopPath, mountDir).Output()
+	require.NoError(t, err, string(mountOut))
+
+	defer func() {
+		require.NoError(t, syscall.Unmount(mountDir, 0))
+	}()
+
+	fn(mountDir)
 }
